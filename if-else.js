b9dc8b4797da755afdb3b632e292b35ead55170a
xonova18 <xonovvacolic@gmail.com>
2023-11-07 16:12:44 +0700

    edit role

--- if-else.js
+++ if-else.js
@@ -1,12 +1,12 @@
 // Proxytia
 let name = "Alfian";
-let role = prompt("masukan rolemu");
+let role = "Tabib";
 
-if(role = Ksatria) {
+if(role == "Ksatria") {
     console.log(`halo ${role} ${name} , kamu dapat menyerang dengan senjatamu!`);
-}else if(role = Tabib) {
+}else if(role == "Tabib") {
     console.log(`halo ${role} ${name} , kamu akan membantu temanmu yang terluka`);
-}else if(role = Penyihir) {
+}else if(role == "Penyihir") {
     console.log(`halo ${role} ${name} , ciptakan keajaiban yang membantu kemenanganmu!`);
 }else {
     console.log(`tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`);
