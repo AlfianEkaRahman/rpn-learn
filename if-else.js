8a3168ed8d781ca36b417e980b616ee689f67f2f
xonova18 <xonovvacolic@gmail.com>
2023-11-07 18:19:48 +0700

    add name validation

--- if-else.js
+++ if-else.js
@@ -1,13 +1,21 @@
 // Proxytia
 let name = "Alfian";
-let peran = "Ksatria";
+let peran = "";
 
-if(peran == "Ksatria") {
-    console.log(`halo ${peran} ${name} , kamu dapat menyerang dengan senjatamu!`);
-}else if(peran == "Tabib") {
-    console.log(`halo ${peran} ${name} , kamu akan membantu temanmu yang terluka`);
-}else if(peran == "Penyihir") {
-    console.log(`halo ${peran} ${name} , ciptakan keajaiban yang membantu kemenanganmu!`);
-}else {
-    console.log(`tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`);
+if (name == "") {
+  console.log(`masukan nama anda`);
+} else if (peran == "Ksatria") {
+  console.log(`halo ${peran} ${name} , kamu dapat menyerang dengan senjatamu!`);
+} else if (peran == "Tabib") {
+  console.log(
+    `halo ${peran} ${name} , kamu akan membantu temanmu yang terluka`
+  );
+} else if (peran == "Penyihir") {
+  console.log(
+    `halo ${peran} ${name} , ciptakan keajaiban yang membantu kemenanganmu!`
+  );
+} else {
+  console.log(
+    `tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`
+  );
 }
