6724b1f58cdc1b7bb0d475b35117560059e9fba1
xonova18 <xonovvacolic@gmail.com>
2023-11-07 16:06:04 +0700

    testing

---
+++ if-else.js
@@ -0,0 +1,13 @@
+// Proxytia
+let name = "Alfian";
+let role = prompt("masukan rolemu");
+
+if(role = Ksatria) {
+    console.log(`halo ${role} ${name} , kamu dapat menyerang dengan senjatamu!`);
+}else if(role = Tabib) {
+    console.log(`halo ${role} ${name} , kamu akan membantu temanmu yang terluka`);
+}else if(role = Penyihir) {
+    console.log(`halo ${role} ${name} , ciptakan keajaiban yang membantu kemenanganmu!`);
+}else {
+    console.log(`tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`);
+}
