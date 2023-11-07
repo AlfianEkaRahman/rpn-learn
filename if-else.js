2124c68700efe2f8b7dc3e558dab2ff60b06ed0a
xonova18 <xonovvacolic@gmail.com>
2023-11-07 16:16:40 +0700

    ganti nama

--- if-else.js
+++ if-else.js
@@ -1,13 +1,13 @@
 // Proxytia
 let name = "Alfian";
-let role = "Tabib";
+let peran = "Tabib";
 
-if(role == "Ksatria") {
-    console.log(`halo ${role} ${name} , kamu dapat menyerang dengan senjatamu!`);
-}else if(role == "Tabib") {
-    console.log(`halo ${role} ${name} , kamu akan membantu temanmu yang terluka`);
-}else if(role == "Penyihir") {
-    console.log(`halo ${role} ${name} , ciptakan keajaiban yang membantu kemenanganmu!`);
+if(peran == "Ksatria") {
+    console.log(`halo ${peran} ${name} , kamu dapat menyerang dengan senjatamu!`);
+}else if(peran == "Tabib") {
+    console.log(`halo ${peran} ${name} , kamu akan membantu temanmu yang terluka`);
+}else if(peran == "Penyihir") {
+    console.log(`halo ${peran} ${name} , ciptakan keajaiban yang membantu kemenanganmu!`);
 }else {
     console.log(`tapi kayaknya kamu jadi bot aja ya, peran yang kamu pilih ga ada`);
 }
