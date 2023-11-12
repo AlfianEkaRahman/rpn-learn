8e257a4aaec9f8f36c02d2187d75efa3168908fd
xonova18 <xonovvacolic@gmail.com>
2023-11-12 18:56:59 +0700

    add array

--- RPN-Phase-0/Array/array.js
+++ RPN-Phase-0/Array/array.js
@@ -18,4 +18,29 @@ function dataHandling(arr) {
 
 dataHandling(input);
 
-//thanks chatgpt hehehe
+
+console.log('=======================================>>>')
+
+
+//thanks chatgpt hehehe tanpamu apa jadinya aku :)
+
+let input1 = ['001', 'Roman Alamsyah', 'Bandar Lampung', '21/05/1989', 'membaca'];
+function dataHandling2(input1){
+    input1.splice(1, 4, 'Roman Alamsyah Elsharawy', 'Provinsi Bandar Lampung', '21/05/1989', 'Pria', 'SMA Internasional Metro');
+    console.log(input1);
+    let splitInput = input1[3].split('/');
+
+    if(splitInput[1] == 5) {
+        console.log('mei');
+    }
+
+    let date = [splitInput[2], splitInput[0], splitInput[1]];
+    console.log(date);
+
+    console.log(splitInput.join(`-`));
+    
+    let slicedInput1 = input1[1].slice(0, 15);
+    console.log(slicedInput1);
+}
+
+dataHandling2(input1);
